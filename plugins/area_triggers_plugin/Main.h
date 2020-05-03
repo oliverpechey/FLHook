1b265ce26724b5cd4356086aafba48f383a7eee0
opechey <olliezeta9@gmail.com>
2020-05-03 14:43:45 +0100

    Removed unecessary imports

--- plugins/area_triggers_plugin/Main.h
+++ plugins/area_triggers_plugin/Main.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <windows.h>
-#include <stdio.h>
-#include <string>
-#include <time.h>
-#include <math.h>
-#include <list>
-#include <map>
-#include <algorithm>
 #include <FLHook.h>
 #include <plugin.h>
 #include "PluginUtilities.h"
